6bb5c2aa8b8997d1c27bf7ac699d0430dfe1f339
Guilherme Neves <guilhermebuenodasneves@gmail.com>
2026-09-16 11:19:44 -0300

    Update URL for html5-qrcode library in service worker

--- sw.js
+++ sw.js
@@ -6,7 +6,7 @@ const ASSETS = [
   './index.html',
   './manifest.json',
   './Tucano-mini.png',
-  'https://unpkg.com/html5-qrcode@2.3.8'
+  'https://unpkg.com/html5-qrcode@2.3.8/html5-qrcode.min.js'
 ];
 
 // 1. Instalação: Salva os arquivos no cache e força a ativação imediata.
